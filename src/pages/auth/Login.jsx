ad9f4b49386d32a0dcdf668b7215b893b140114f
Hardik <hardikb.bheemainfotech@gmail.com>
2026-03-12 16:39:27 +0530

    Route change

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -110,7 +110,7 @@ export default function Login() {
               L
             </div>
             <span className="text-xl font-black text-slate-900">
-              LearnX
+              Learn
             </span>
           </div>
 
